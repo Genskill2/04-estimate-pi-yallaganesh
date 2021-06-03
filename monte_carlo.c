16f557b80aa86fab2cafcb25c725fac8ce908512
yallaganesh <84661582+yallaganesh@users.noreply.github.com>
2021-06-03 11:01:24 +0530

    Update monte_carlo.c

--- monte_carlo.c
+++ monte_carlo.c
@@ -38,6 +38,18 @@ int main(void) {
     }
   }
 }
-
-
+ float mc_pi(int n)
+ {
+    int incircle=0;
+  for(int i=0;i<n;i++){
+    float x=frandom();
+    float y=frandom();
+    float d=x*x+y*y;
+    if(d<=1){
+      incircle++;
+    }
+  }
+   float pi =4*(float)incircle/n;
+   return pi;
+ }
 
