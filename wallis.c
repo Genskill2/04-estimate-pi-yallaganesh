7a958baf82c93bee04ef44b3be24483f64a1cb0f
yallaganesh <84661582+yallaganesh@users.noreply.github.com>
2021-06-03 11:11:52 +0530

    Update wallis.c

--- wallis.c
+++ wallis.c
@@ -23,12 +23,11 @@ int main(void) {
     }
   }
 }
-
-foat wallis_pi(int count){
-  float pi=10;
-  for(int i=1;i<count;i++){
-    flkoat num= 4.0*i*i;
-    pi*=num/(num-1);
-  }
-  return pi*2;
-}
+float wallis_pi(int count){
+    float pi = 1.0;
+    for (int i = 1; i < count; i++){
+        float num = 4.0 * i * i;
+        pi *= num / (num - 1);
+    }
+    return pi * 2;
+ }
